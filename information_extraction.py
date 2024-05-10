370526f30d78afcd8f1ccc13477353aa72d5c3b6
eric-huyl <2069280013@qq.com>
2024-05-10 19:45:39 +0800

    added submodule

--- information_extraction.py
+++ information_extraction.py
@@ -2,10 +2,17 @@ import split_sentence
 
 
 def simplify_entity(file_path):
+    edges = []
     content = split_sentence.read_from_json(file_path)
     for sentence in content:
-        print(sentence['output'])
+        entity_edge = []
+        for entity in sentence['output']:
+            reduced_entity = [entity['type'], entity['span']]
+            entity_edge.append(reduced_entity)
+        edges.append(entity_edge)
+    return edges
 
 
 if __name__ == '__main__':
-    simplify_entity('recognized_entities.json')
+    split_sentence.save_to_json(simplify_entity('recognized_entities.json'),
+                                'raw_edges.json')
